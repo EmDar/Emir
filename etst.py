6ae5c3a47ff8bd332b15be896bbe30ae4c3dae30
EmDar <134640033+EmDar@users.noreply.github.com>
2023-06-02 15:27:57 +0700

    Update etst.py

--- etst.py
+++ etst.py
@@ -5,4 +5,4 @@ response = requests.get('http://localhost:5000')
 if response.status_code == 200:
     print('Success! The server responded with:', response.text)
 else:
-    print('Oh no! Something went wrong.')
+    print('Oh no! Something went wrong.'
